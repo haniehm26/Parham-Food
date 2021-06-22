1fb5e07c4ae3e4707e88a5f2f3ff47550fd73267
Hanieh <haniehmahdavi@yahoo.com>
2021-06-22 23:18:44 +0430

    add login and signup for manager

--- app/resources/manager/auth.py
+++ app/resources/manager/auth.py
@@ -4,8 +4,8 @@ from flask_jwt_extended import create_access_token
 from pymongo.errors import CollectionInvalid
 import datetime
 from database.db import mongo
-from database.hashing import hash_password
-from resources.errors import EmailAlreadyExistsError, SchemaValidationError
+from database.hashing import hash_password, check_password
+from resources.errors import EmailAlreadyExistsError, SchemaValidationError, UserNotExistsError, UnauthorizedError
 
 class SignupApi(Resource):
     def post(self):
@@ -21,6 +21,25 @@ class SignupApi(Resource):
                 new_manager = managers.find_one({'_id': manager_id})
                 expires = datetime.timedelta(days=7)
                 access_token = create_access_token(identity=str(manager_id), expires_delta=expires)
+            return {'token': access_token}, 200
+
+        except CollectionInvalid:
+            raise SchemaValidationError
+
+class LoginApi(Resource):
+    def post(self):
+        try:
+            managers = mongo.db.managers
+            body = request.get_json()
+            manager_found = managers.find_one({'email': body['email']})
+            if not manager_found:
+                raise UserNotExistsError
+            else:
+                authorized = check_password(manager_found['password'], body['password'])
+                if not authorized:
+                    raise UnauthorizedError
+                expires = datetime.timedelta(days=7)
+                access_token = create_access_token(identity=str(manager_found['_id']), expires_delta=expires)
             return {'token': access_token}, 200
 
         except CollectionInvalid:
--- app/resources/routes.py
+++ app/resources/routes.py
@@ -1,15 +1,16 @@
-from resources.manager.auth import SignupApi
+from resources.manager.auth import SignupApi, LoginApi
 from resources.restaurant.register import RegisterRestaurantApi
 from resources.restaurant.edit import EditRestaurantApi
 from resources.restaurant.get_all import AllRestaurantsApi
 from resources.food.add_item import AddFoodItemApi
 
 def initialize_routes(api):
-    api.add_resource(SignupApi, '/manager/login')  # post
+    api.add_resource(SignupApi, '/manager/signup')  # post
+    api.add_resource(LoginApi, '/manager/login')  # post
     api.add_resource(RegisterRestaurantApi, '/restaurant')  # post
     api.add_resource(AllRestaurantsApi, '/restaurant')  # get
     api.add_resource(EditRestaurantApi, '/restaurant')  # put
-    api.add_resource(AddFoodItemApi, '/restaurant/foods')  # post
+    # api.add_resource(AddFoodItemApi, '/restaurant/foods')  # post
     # api.add_resource(, '/restaurant/<id>/foods/<id>')  # delete
     # api.add_resource(, '/restaurant/<id>/foods/<id>')  # put
     # api.add_resource(, '/orders/<id>')  # put
