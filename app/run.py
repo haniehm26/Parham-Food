11fe144f1941747830d1637a8179da813cfe48e8
Hanieh <haniehmahdavi@yahoo.com>
2021-06-22 13:24:14 +0430

    modify run.py

--- app/run.py
+++ app/run.py
@@ -1,9 +1,4 @@
 from app import app
-import sys
-
-path = '/home/haniehm26/mysite'
-if path not in sys.path:
-   sys.path.insert(0, path)
 
 if __name__ == '__main__':
     # this is used to run the flask app
